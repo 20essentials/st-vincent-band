3f32256676e9138a5283aaa40f8aa7384df0ed7a
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:21:03 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ST. VINCENT';
+export const nameSongs = [
+	"LOS AGELESS", /*1) (8.0)*/
+	"SLOW SLOW DISCO", /*2*/
+	"SAVIOR", /*3*/
+	"MASSEDUCTION", /*4*/
+	"SUGARBOY", /*5*/
+	"FEAR THE FUTURE", /*6*/
+	"SMOKING SECTION", /*7*/
+	"LOS AGELESS (PIANO VERSION)", /*8*/
+	"NEW YORK", /*9*/
+	"YOUNG LOVER", /*10*/
+	"HAPPY BIRTHDAY JOHNNY", /*11*/
+	"PILLS", /*12*/
+	"HANG ON ME", /*13*/
+	"NOW, NOW", /*14*/
+	"YOUR LIPS ARE RED", /*15*/
+	"MARRY ME", /*16*/
+	"PARIS IS BURNING", /*17*/
+	"THE APOCALYPSE SONG", /*18*/
+	"BIRTH IN REVERSE", /*19*/
+	"WHAT ME WORRY", /*20*/
+]
